9740d307b2d9bac6b9cd4a2868513375f688ad4f
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-12-08 16:04:20 +0530

    added post model

---
+++ server/src/models/post-model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    content: {
+      type: String,
+    },
+    media: {
+      type: String,
+    },
+    public_id: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("post", postSchema);
+
+module.exports = Post;
