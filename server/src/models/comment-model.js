ba538fc19868ea2b8587c29d710ffe9fa98e32fb
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-12-08 16:09:11 +0530

    added comment model

---
+++ server/src/models/comment-model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+    text: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("comment", commentSchema);
+
+module.exports = Comment;
