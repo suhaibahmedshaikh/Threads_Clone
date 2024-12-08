16122e734d1644b87962754e1fe80cafdc67a4f6
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-12-08 15:56:08 +0530

    added user model

---
+++ server/src/models/user-model.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    bio: {
+      type: String,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://www.pngall.com/wp-content/uploads/15/User-PNG-Free-Image.png",
+    },
+    public_id: {
+      type: String,
+    },
+    follower: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    threads: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    repies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comment",
+      },
+    ],
+    repost: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
