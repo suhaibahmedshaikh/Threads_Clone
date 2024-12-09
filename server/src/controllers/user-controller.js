9aa16153216cb6315ae6b98b20a53369445c7cd7
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-12-09 23:05:24 +0530

    added signup and signin user controller

---
+++ server/src/controllers/user-controller.js
@@ -0,0 +1,114 @@
+const User = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/envConfig");
+
+const signup = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({
+        msg: "first name, email and password are required",
+      });
+    }
+
+    const userExits = await User.findOne({ email });
+
+    if (userExits) {
+      return res.status(400).json({
+        msg: "user already exits",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    if (!hashedPassword) {
+      return res.status(400).json({
+        msg: "error while hashing password",
+      });
+    }
+
+    const newUser = await User.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (!newUser) {
+      return res.status(400).json({
+        msg: "error while registering a user",
+      });
+    }
+
+    res.status(201).json({
+      msg: `User registered successfully, Welcome ${userExits?.firstName}`,
+    });
+  } catch (err) {
+    res.status(400).json({
+      msg: "Error while signup !",
+      err: err.message,
+    });
+  }
+};
+
+const signin = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        msg: "email and password are required",
+      });
+    }
+
+    const userExits = await User.findOne({ email });
+
+    if (!userExits) {
+      return res.status(400).json({
+        msg: "user doesn't exits, please signup",
+      });
+    }
+
+    const matchedPassword = await bcrypt.compare(password, userExits.password);
+
+    if (!matchedPassword) {
+      return res.status(400).json({
+        msg: "invalid credentials",
+      });
+    }
+
+    const accessToken = jwt.sign({ token: userExits._id }, JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    if (!accessToken) {
+      return res.status(400).json({
+        msg: "error while generating login token",
+      });
+    }
+
+    res.cookie("token", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    res.status(200).json({
+      msg: `User logged in successfully, Hello ${userExits.firstName}`,
+    });
+  } catch (err) {
+    res.status(400).json({
+      msg: "Error while signin !",
+      err: err.message,
+    });
+  }
+};
+
+module.exports = { signup, signin };
