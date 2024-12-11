7188aeca36630217bcc85ada04b7d3b82c566207
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-12-11 15:14:00 +0530

    added cloudinary config

--- server/src/config/envConfig.js
+++ server/src/config/envConfig.js
@@ -4,4 +4,7 @@ module.exports = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
