bbee9d504510ceedd19c644d7d88065bcf37f14b
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-12-11 16:40:47 +0530

    added post router

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const { PORT } = require("./config/envConfig");
 const connectDB = require("./config/dbConfig");
 const userRouter = require("./routes/user-routes");
+const postRouter = require("./routes/post-router");
 
 // configurations
 connectDB();
@@ -16,7 +17,8 @@ app.use(cookieParser());
 
 // routes
 app.use("/api/v1/user", userRouter);
-
+app.use("/api/v1/post", postRouter);
+src/index.jssrc/index.js
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
